49c2d37aa9a2d27fd81ec0cb6f4ca3b6ae9b282c
nada-mah <cutenada.lara4@gmail.com>
2022-08-29 20:20:22 +0200

    downgrade node

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://hostfullstack-env.eba-4mdhzqkk.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-env.eba-qtr2zec8.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
